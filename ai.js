537880c6b05c7e417f70980bb9b2388f5db73984
Kevin thapa <bobmarley9461238@gmail.com>
2025-12-31 19:14:57 +0000

    backend

--- ai.js
+++ ai.js
@@ -1,6 +1,9 @@
-import { HfInference } from "@huggingface/inference";
+import { HfClient } from "@huggingface/inference";
 
-const hf = new HfInference(process.env.HF_ACCESS_TOKEN);
+const client = new HfClient({
+    apiKey: process.env.HF_ACCESS_TOKEN
+})
+
 
 export async function getRecipeFromMistral(ingredientsArr) {
   const ingredientsString = ingredientsArr.join(", ");
